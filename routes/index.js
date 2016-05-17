00f7fe629b4652b2017cd99f56b2629a5a89160b
AkyunaAkish <akyunaakish@gmail.com>
2016-05-16 21:45:36 -0600

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('people.txt', 'utf8', function(err,data){
+    var people = data.split('\n');
+    res.render('index', {people: people});
+  });
 });
+
+router.post('/', function(req,res,next){
+  if (req.body.name) {
+    fs.appendFile('people.txt', req.body.name + '\n', function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect('/');
+      }
+    })
+  } else {
+    fs.readFile('people.txt', 'utf8', function(err,data){
+      var people = data.split('\n');
+      res.render('index', {error: 'No name provided', people: people});
+    });
+  }
+});
+
 
 module.exports = router;
